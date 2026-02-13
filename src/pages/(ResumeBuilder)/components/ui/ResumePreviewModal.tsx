5e33d768d6dc9ed290d86563224fde90ff6c4b38
kishor2121 <kishorkumarhkumar@gmail.com>
2026-02-13 12:06:54 +0530

    minor code fixes

--- src/pages/(ResumeBuilder)/components/ui/ResumePreviewModal.tsx
+++ src/pages/(ResumeBuilder)/components/ui/ResumePreviewModal.tsx
@@ -843,7 +843,7 @@ const ResumePreviewModal: React.FC<ResumePreviewModalProps> = ({
                   {/* Header with Resume Name */}
                   <div className="flex items-center justify-between mb-4 pb-4 border-b border-gray-200">
                     <h3 className="text-lg font-semibold text-gray-900">
-                      PDF Preview
+                      Bowizzy Preview
                     </h3>
                   </div>
 
